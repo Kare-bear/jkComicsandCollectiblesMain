8a29dac7cca37203306bb8ab5a94095482003beb
Kara Rainey <kara@brivity.com>
2022-01-06 17:49:38 -0600

    updated small amout of css for nav bar after links were added

--- src/Components/CategoryNav/CategoryNav.js
+++ src/Components/CategoryNav/CategoryNav.js
@@ -7,13 +7,13 @@ import { useNavigate } from "react-router-dom";
       return(
         <div>
           <div id="boxGroup">
-            <button id="testerFont" onClick={()=>{navigate("/category-page");}}><p id="stationaryBox">PreOrders</p></button>
-            <button id="testerFont" onClick={()=>{navigate("/category-page");}}><p id="stationaryBox">Transformers</p></button>
-            <button id="testerFont" onClick={()=>{navigate("/category-page");}}><p id="stationaryBox">New Arrivals</p></button>
-            <button id="testerFont" onClick={()=>{navigate("/category-page");}}><p id="stationaryBox">Pokemon</p></button>
-            <button id="testerFont" onClick={()=>{navigate("/category-page");}}><p id="stationaryBox">Marvel</p></button>
-            <button id="testerFont" onClick={()=>{navigate("/category-page");}}><p id="stationaryBox">Star Wars</p></button>
-            <button id="testerFont" onClick={()=>{navigate("/category-page");}}><p id="stationaryBox">Power Rangers</p></button>
+            <button id="categoryNav" onClick={()=>{navigate("/category-page");}}><p id="stationaryBox">PreOrders</p></button>
+            <button id="categoryNav" onClick={()=>{navigate("/category-page");}}><p id="stationaryBox">Transformers</p></button>
+            <button id="categoryNav" onClick={()=>{navigate("/category-page");}}><p id="stationaryBox">New Arrivals</p></button>
+            <button id="categoryNav" onClick={()=>{navigate("/category-page");}}><p id="stationaryBox">Pokemon</p></button>
+            <button id="categoryNav" onClick={()=>{navigate("/category-page");}}><p id="stationaryBox">Marvel</p></button>
+            <button id="categoryNav" onClick={()=>{navigate("/category-page");}}><p id="stationaryBox">Star Wars</p></button>
+            <button id="categoryNav" onClick={()=>{navigate("/category-page");}}><p id="stationaryBox">Power Rangers</p></button>
           </div>      
         </div>
       )
